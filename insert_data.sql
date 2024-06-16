0ac77b00a9981edf7b4929796468305b5d58d783
bellouch aissam <bellouchaissam@gmail.com>
2024-06-16 18:05:31 +0200

    Ajout des fichiers SQL de la base de données tifosi

---
+++ insert_data.sql
@@ -0,0 +1,13 @@
+USE tifosi;
+
+INSERT INTO focaccia (name, description) VALUES ('Focaccia Classique', 'Focaccia avec huile d\'olive');
+INSERT INTO focaccia (name, description) VALUES ('Focaccia Tomate', 'Focaccia avec tomates fraîches');
+
+INSERT INTO ingredient (name) VALUES ('Basilic');
+INSERT INTO ingredient (name) VALUES ('Huile d\'olive');
+
+INSERT INTO marque (name) VALUES ('San Pellegrino');
+INSERT INTO marque (name) VALUES ('Coca-Cola');
+
+INSERT INTO boisson (name) VALUES ('Eau gazeuse');
+INSERT INTO boisson (name) VALUES ('Soda');
